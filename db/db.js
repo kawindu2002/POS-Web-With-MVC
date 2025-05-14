37402333cbcd2f7c302e2868ce3b2b670827e837
Kavindu Priyashan <kawindupriyashan@gmail.com>
2025-05-14 22:24:33 +0530

    remove or.history data from db.js

--- db/db.js
+++ db/db.js
@@ -12,28 +12,5 @@ export let items_db = [
     { code: 'I004', name: 'Monitor', description: '27-inch 4K monitor', price: 199.99, quantity: 10 },
 ];
 
-export let orders_db = [
-        {
-            orderId: 'ORD001',
-            date: '2023-10-26',
-            customerId: 'C001',
-            customerName: 'Pasindu Malshan',
-            total: 1251.00,
-            items: [
-                { code: 'I001', name: 'Laptop', price: 1200.00, quantity: 1 }
-            ]
-        },
-
-        {
-            orderId: 'ORD002',
-            date: '2023-10-25',
-            customerId: 'C002',
-            customerName: 'Shanuka Sathsara',
-            total: 500.00,
-            items: [
-                { code: 'I004', name: 'Monitor', price: 199.99, quantity: 2 },
-                { code: 'I002', name: 'Mouse', price: 25.50, quantity: 4 }
-            ]
-        }
-];
+export let orders_db = [];
 
